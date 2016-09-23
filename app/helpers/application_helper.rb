9298360da916e05f5749487ffabae3ae0b3e0eff
Rhadax <n.can2lon@gmail.com>
2016-09-23 22:28:42 +0200

    finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return a basic title if is empty
+  def full_title(page_title = "")
+    base_title = "My first app in Ruby on Rails"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
